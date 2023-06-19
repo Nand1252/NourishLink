840d12d943a8eba913ef37626909a1446de63cbf
anshkyt <ansh.krishna1@gmail.com>
2023-06-19 11:19:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
 
-console.log("hello");
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
