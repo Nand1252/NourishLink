9b3a4ee272cd185e21ee3b4a0e31037dd6b2fc77
Nand1252 <nandanr100@gmail.com>
2023-06-22 22:51:53 -0400

    Donate page updates
    Added thank you message logic.

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(express.static('public'));
 
 const server = http.createServer(app);
 
-const io = socketIO(server);
-app.set('io', io);
-
 let foodListHtml = fs.readFileSync('./public/browse.html', 'utf8');
 
 // In-memory storage for food items
@@ -41,19 +38,6 @@ app.get('/donate', (req, res) => {
   res.sendFile(__dirname + '/public/donate.html');
 });
 
-app.get('/request/:id', (req, res) => {
-  const itemId = req.params.id;
-  const item = foodItems.find(item => item.id === itemId);
-
-  if (!item || item.status !== 'available') {
-    res.send(`
-      <h2>Food item not found or not available!</h2>
-    `);
-  } else {
-    res.sendFile(__dirname + '/public/request.html');
-  }
-});
-
 // API endpoint to create a new food donation
 app.post('/donate', (req, res) => {
   const { title, description, quantity } = req.body;
@@ -78,34 +62,8 @@ app.post('/donate', (req, res) => {
   fs.writeFile('./public/data.json', JSON.stringify(newFoodItem, null, 2), (err) => {
     if (err) throw err;
   });
-
-  // Emit an event to notify clients about the new food item
-  const io = req.app.get('io');
-  io.emit('newFoodItem', newFoodItem);
+  
   res.sendFile(__dirname + '/public/donate.html');
-});
-
-// API endpoint to request a food item
-app.post('/request/:id', (req, res) => {
-  const itemId = req.params.id;
-  const requesterName = req.body.name;
-
-  const item = foodItems.find(item => item.id === itemId);
-
-  if (!item || item.status !== 'available') {
-    res.send(`
-      <h2>Food item not found or not available!</h2>
-    `);
-  } else {
-    item.status = 'requested';
-    item.requesterName = requesterName;
-
-    res.send(`
-      <h2>Food item requested successfully!</h2>
-      <p>Item ID: ${itemId}</p>
-      <p>Requester Name: ${requesterName}</p>
-    `);
-  }
 });
 
 // Serve browse.html for browsing available food items
